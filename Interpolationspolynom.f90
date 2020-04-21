601aef10fa6196493832011c6a3214b4b9a176f0
Laurenz <laurenz_ruzicka@gmx.at>
2020-04-21 14:08:39 +0200

    Projektdateien hinzufügen.

---
+++ Interpolationspolynom.f90
@@ -0,0 +1,25 @@
+!  Interpolationspolynom.f90 
+!
+!  FUNCTIONS:
+!  Interpolationspolynom - Entry point of console application.
+!
+
+!****************************************************************************
+!
+!  PROGRAM: Interpolationspolynom
+!
+!  PURPOSE:  Entry point for the console application.
+!
+!****************************************************************************
+
+    program Interpolationspolynom
+
+    implicit none
+
+    ! Variables
+
+    ! Body of Interpolationspolynom
+    print *, 'Hello World'
+
+    end program Interpolationspolynom
+
